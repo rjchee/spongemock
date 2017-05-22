93be9225da408ec6619e81ec896f44818b886698
Raymond Chee <rjchee@gmail.com>
2017-05-22 18:57:28 -0500

    Add postgres library

--- cmd/worker/globals.go
+++ cmd/worker/globals.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/url"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 var (
 	AppURL  string
 	IconURL string
 	MemeURL string
+	DB      *sql.DB
 )
 
 const (
@@ -40,6 +44,19 @@ func init() {
 	IconURL = u.ResolveReference(icon).String()
 	meme, _ := url.Parse(memePath)
 	MemeURL = u.ResolveReference(meme).String()
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL != "" {
+		DB, err = sql.Open("postgres", dbURL)
+		if err != nil {
+			log.Println("DATABASE_URL is not set")
+			DB = nil
+		} else if err = DB.Ping(); err != nil {
+			log.Println("error pinging the database:", err)
+			log.Println("closing database connection:", DB.Close())
+			DB = nil
+		}
+	}
 }
 
 func SetEnvVariable(name string, value *string) {

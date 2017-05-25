97a697eeaea9d07dcf6803606670ba4cc961e4a7
Raymond Chee <rjchee@gmail.com>
2017-05-24 23:46:56 -0500

    Add DB initialization to the web dyno

--- cmd/spongemock/globals.go
+++ cmd/spongemock/globals.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strings"
+
+	_ "github.com/lib/pq"
 )
 
 var (
 	AppURL  string
 	IconURL string
 	MemeURL string
+	DB      *sql.DB
 	DEBUG   bool
 )
 
@@ -44,6 +48,19 @@ func init() {
 	IconURL = u.ResolveReference(icon).String()
 	meme, _ := url.Parse(memePath)
 	MemeURL = u.ResolveReference(meme).String()
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL != "" {
+		DB, err = sql.Open("postgres", dbURL)
+		if err != nil {
+			log.Println("DATABASE_URL is not set")
+			DB = nil
+		} else if err = DB.Ping(); err != nil {
+			log.Println("error pinging the database:", err)
+			log.Println("closing database connection:", DB.Close())
+			DB = nil
+		}
+	}
 
 	DEBUG = strings.ToLower(os.Getenv("DEBUG")) != "false"
 	if DEBUG {
